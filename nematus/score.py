6800573b4afefeb5978b88c07ed2d40e359844d3
Rachel Bawden <rachel.bawden@limsi.fr>
2017-08-07 14:58:07 +0100

    start debug scorer

--- nematus/score.py
+++ nematus/score.py
@@ -37,8 +37,9 @@ def load_scorer(model, option, alignweights=None):
         trng, use_noise, x, x_mask, aux_x, aux_x_mask, y, y_mask, opt_ret, cost = build_multisource_model(tparams, option)
         inps = [x, x_mask, aux_x, aux_x_mask, y, y_mask]
 
-
-    print(inps)
+    for inp in inps:
+        print(inp)
+        print(inp.tag.test_value.shape)
 
     use_noise.set_value(0.)
 
