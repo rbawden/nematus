dd62cdbfc4898e8cbaa10cbfd16a4e87d286eeed
Rachel Bawden <rachel.bawden@limsi.fr>
2017-08-14 17:01:51 +0100

    made scorer multisoruce

--- nematus/score.py
+++ nematus/score.py
@@ -32,7 +32,9 @@ def load_scorer(model, option, alignweights=None):
     params = load_params(model, param_list)
     tparams = init_theano_params(params)
 
-    if params['multisource_type'] is None:
+    print(params)
+
+    if 'multisource_type' not in params or params['multisource_type'] is None:
         print("building single source model")
         trng, use_noise, x, x_mask, y, y_mask, opt_ret, cost = build_model(tparams, option)
         inps = [x, x_mask, y, y_mask]
@@ -44,7 +46,7 @@ def load_scorer(model, option, alignweights=None):
 
     if alignweights:
         logging.debug("Save weight mode ON, alignment matrix will be saved.")
-        if params['multisource_type'] is not None:
+        if 'multisource_type' not in params or params['multisource_type'] is not None:
             outputs = [cost, opt_ret['dec_alphas'], opt_ret['dec_alphas2']]
             f_log_probs = theano.function(inps, outputs)
         else:
