eff41f8ef3312e57c1ec68c01f91d79d0a991bfd
Rachel Bawden <rachel.bawden@limsi.fr>
2017-10-13 18:24:15 +0200

    forgotten suffix

--- nematus/layers.py
+++ nematus/layers.py
@@ -1176,8 +1176,8 @@ def tri_gru_cond_layer(tparams, state_below, options, dropout, prefix='gru',
         pctx_ = tensor.dot(context * ctx_dropout[0][0], wn(pp(prefix, 'Wc_att0'))) + \
                     tparams[pp(prefix, 'b_att0')]
     if options['layer_normalisation']:
-        pctx_ = layer_norm(pctx_, tparams[pp(prefix, 'Wc_att_lnb')],
-                               tparams[pp(prefix, 'Wc_att_lns')])
+        pctx_ = layer_norm(pctx_, tparams[pp(prefix, 'Wc_att_lnb0')],
+                               tparams[pp(prefix, 'Wc_att_lns0')])
 
     # 2nd context
     ctx_dropout.append(dropout((n_samples, 2 * options['dim']), dropout_probability_ctx, num=5))
