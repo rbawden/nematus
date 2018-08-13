6d2112b3602dd28f08167134c4b5dd4d052676df
rbawden <rachieb@gmail.com>
2018-08-13 10:47:39 +0200

    Create test_multitranslate.sh

---
+++ test/test_multitranslate.sh
@@ -0,0 +1,12 @@
+NEMATUS=PATH_TO/nematus-multisource/nematus
+SRC=de
+TRG=en
+modelname=~/path/to/model/model.npz
+test=./data/corpus.5
+output=OUTPUT_FILENAME
+# decode                                                                                           $
+
+python $NEMATUS/translate.py \
+    -m $modelname \
+    -i $test.$SRC -o $output.$TRG -k 5 -n -p 2 --suppress-unk \
+    --aux_input $test.sm1.$SRC \
